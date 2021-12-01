729f78dafc8c9da5b7152f11355769ac76c49890
FerrarisEdoardo <91604126+FerrarisEdoardo@users.noreply.github.com>
2021-12-01 20:59:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+
 
 int main(){
+  int a=0;
+  int risultato;
+  cin >>a;
+  risultato=fun(a);
+  if (risultato ==1){
+    cout<<"numero primo" << endl;
+  }else{
+    cout <<"numero non primo" << endl;
+  }
   return 0;
 }
