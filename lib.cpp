f4f22cf7141d9e00742f9021657b30a5fa1003bd
FerrarisEdoardo <91604126+FerrarisEdoardo@users.noreply.github.com>
2021-12-01 21:02:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include 2lib.h"
+int fun (int a){
+  int div=1, divint=0;
+  while(divint<=1 and div <=a/2){
+    if (a%div==0){
+      divint++;
+    }
+    div++;
+  }if (divint==1){
+    return 1;
+  }else{
+    return 0;
+  }
+}
